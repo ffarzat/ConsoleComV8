165e67b66a1b44215ef60b31bee68526be26cd25
ffarzat <fabiofarzat@gmail.com>
2015-09-06 09:19:46 -0300

    Ätualizando o console

--- ConsoleComV8/Program.cs
+++ ConsoleComV8/Program.cs
@@ -8,25 +8,33 @@ using System.Threading.Tasks;
 using ClearScript.Manager;
 using Microsoft.ClearScript.V8;
 using NLog;
+using Otimizacao.Javascript;
 
 
 namespace ConsoleComV8
 {
+
+    /// <summary>
+    /// Console de Execução
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Principal
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            var helperMoment = new JavascriptHelper("Moment", false, false);
+            helperMoment.ExecutarTestes("global.js", "core-test.js");
 
-            ManagerPool.InitializeCurrentPool(new ManualManagerSettings { RuntimeMaxCount = 2 });
+            var helperLodash = new JavascriptHelper("Lodash", true, false);
+            helperLodash.ExecutarTestes("lodash.js", "lodashTest.js");
 
-            using (var scope = new ManagerScope())
-            {
-                var engine = scope.RuntimeManager.GetEngine();
-                engine.Execute("var i = 200;");
-                Console.WriteLine(engine.Script.i);
-            }
+            var helperUnderscore = new JavascriptHelper("underscore", true, false);
+            helperUnderscore.ExecutarTestes("underscore.js", "underscoreTests.js");
 
-
+            Console.Read();
         }
 
     }
