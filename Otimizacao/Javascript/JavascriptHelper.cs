8892b3244089cfe24272fe07f96ae79fdf4a9ae9
ffarzat <fabiofarzat@gmail.com>
2015-09-13 12:20:29 -0300

    Fit again

--- Otimizacao/Javascript/JavascriptHelper.cs
+++ Otimizacao/Javascript/JavascriptHelper.cs
@@ -677,7 +677,7 @@ namespace Otimizacao.Javascript
             //this.FalhasDosTestes.ForEach(this.Log);
 
             if (TestesComFalha > 0)
-                return _fitTopValue + TestesComFalha;
+                return _fitTopValue + 1000;
             if (TestesComSucesso == 0)
                 return _fitTopValue + 1000;
             if (TestesComSucesso != TotalTestes)
