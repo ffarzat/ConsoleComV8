05be8bf5d15f32b1d4dfeb16bb827887104864de
ffarzat <fabiofarzat@gmail.com>
2015-09-21 18:06:29 -0300

    Deus!

--- ConsoleComV8/Program.cs
+++ ConsoleComV8/Program.cs
@@ -29,19 +29,19 @@ namespace ConsoleComV8
 
             #region Moment
 
-            var otimizadorMoment = new Otimizador(100, 100, 10, "Moment", "ResultadosMoment");
-            otimizadorMoment.LimparResultadosAnteriores();
+            //var otimizadorMoment = new Otimizador(100, 100, 10, "Moment", "ResultadosMoment");
+            //otimizadorMoment.LimparResultadosAnteriores();
 
-            var otimizouMoment = otimizadorMoment.Otimizar("global.js", "core-test.js");
+            //var otimizouMoment = otimizadorMoment.Otimizar("global.js", "core-test.js");
 
-            Console.WriteLine("{0} otimizou? {1}", "Moment", otimizouMoment);
+            //Console.WriteLine("{0} otimizou? {1}", "Moment", otimizouMoment);
 
             #endregion
 
-            GC.Collect();
-            GC.WaitForPendingFinalizers();
-            GC.WaitForFullGCComplete(60000);
-            GC.Collect();
+            //GC.Collect();
+            //GC.WaitForPendingFinalizers();
+            //GC.WaitForFullGCComplete(60000);
+            //GC.Collect();
 
             #region Lodash
 
