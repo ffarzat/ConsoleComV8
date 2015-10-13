cfd06e814a6b1a8ef3e823be128feb9f5e1c53ab
ffarzat <fabiofarzat@gmail.com>
2015-10-13 10:29:23 -0300

    retirando o Moment das rodadas

--- ConsoleComV8/Program.cs
+++ ConsoleComV8/Program.cs
@@ -64,13 +64,13 @@ namespace ConsoleComV8
             Console.WriteLine("======================================   Rodada {0}", rodada);
             #region Moment
 
-            var otimizadorMoment = new Otimizador(100, 100, 10, "Moment", "ResultadosMoment");
-            otimizadorMoment.ConfigurarRodada(rodada);
-            otimizadorMoment.LimparResultadosAnteriores();
+            //var otimizadorMoment = new Otimizador(100, 100, 10, "Moment", "ResultadosMoment");
+            //otimizadorMoment.ConfigurarRodada(rodada);
+            //otimizadorMoment.LimparResultadosAnteriores();
 
-            var otimizouMoment = otimizadorMoment.Otimizar("global.js", "core-test.js");
+            //var otimizouMoment = otimizadorMoment.Otimizar("global.js", "core-test.js");
 
-            Console.WriteLine("{0} otimizou? {1}", "Moment", otimizouMoment);
+            //Console.WriteLine("{0} otimizou? {1}", "Moment", otimizouMoment);
 
             #endregion
 
