6f24b889c77719265f98ee0476669dc5feda5bc7
ffarzat <fabiofarzat@gmail.com>
2016-01-15 19:17:27 -0200

    Iniciando o tratamento dos diretrios e arquivos

--- GeradorExcelAnalitico/Program.cs
+++ GeradorExcelAnalitico/Program.cs
@@ -7,11 +7,19 @@ using System.Threading.Tasks;
 
 namespace GeradorExcelAnalitico
 {
+    /// <summary>
+    /// Inicial
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Principal
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
 
+            //Diretório do experimento (raiz)
             var fromDirectoryPath = args[0];
 
             if(string.IsNullOrEmpty(fromDirectoryPath))
@@ -26,9 +34,48 @@ namespace GeradorExcelAnalitico
                 Environment.Exit(-1);
             }
 
-            
+            ValidarDiretorios(fromDirectoryPath);
+            ProcessarDiretorios(fromDirectoryPath);
+        }
 
+        /// <summary>
+        /// Processa os arquivos em excel, conta os arquivos em Js e gera o relatório final
+        /// </summary>
+        /// <param name="fromDirectoryPath"></param>
+        private static void ProcessarDiretorios(string fromDirectoryPath)
+        {
+            var dir = new DirectoryInfo(fromDirectoryPath);
+            foreach (var subdir in dir.GetDirectories())
+            {
+                Console.WriteLine("     Processando : {0}", subdir.Name);
 
+                //GA
+
+                //HC
+                
+            }
+        }
+
+        /// <summary>
+        /// Varre a estrutura e descobre as bibliotecas
+        /// </summary>
+        /// <param name="fromDirectoryPath"></param>
+        private static void ValidarDiretorios(string fromDirectoryPath)
+        {
+            var dir = new DirectoryInfo(fromDirectoryPath);
+            int totalLibraries = dir.GetDirectories().Count();
+            Console.WriteLine("{0} de bibliotecas para processar",totalLibraries);
+
+            foreach (var subdir in dir.GetDirectories())
+            {
+                Console.WriteLine("     {0}", subdir.Name);
+
+                if (subdir.GetDirectories().Count() !=2)
+                {
+                    Console.WriteLine("Diretório da biblioteca fora do formato esperado");
+                    Environment.Exit(-1); 
+                }
+            }
         }
     }
 }
