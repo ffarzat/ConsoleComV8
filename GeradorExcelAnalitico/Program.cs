4a9e2a41f5a9eaf9906e9cdcbd88456a9a3c2452
ffarzat <fabiofarzat@gmail.com>
2016-01-15 18:59:04 -0200

    Incluindo novo projeto para processar as planilhas

---
+++ GeradorExcelAnalitico/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeradorExcelAnalitico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var fromDirectoryPath = args[0];
+
+            if(string.IsNullOrEmpty(fromDirectoryPath))
+            {    
+                Console.WriteLine("Diretório do experimento não informado");
+                Environment.Exit(-1);
+            }
+
+
+
+
+        }
+    }
+}
