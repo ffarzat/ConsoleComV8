1a34baf90b4cdaff1f2dd1b1b676d1a58264062c
ffarzat <fabiofarzat@gmail.com>
2016-02-19 18:37:41 -0200

    Aumentano para 20 avaliaçoes por individuo

--- Otimizacao/Otimizador.cs
+++ Otimizacao/Otimizador.cs
@@ -847,7 +847,7 @@ namespace Otimizacao
         [HandleProcessCorruptedStateExceptions]
         private double AvaliarIndividuo(int indice, Individuo sujeito)
         {
-            const int total = 5;
+            const int total = 20;
             var fits = new double[total];
 
             fits[0] = ExecutarTestesParaIndividuoEspecifico(indice, sujeito);
